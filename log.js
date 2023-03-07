de5f0764589059d3beca5e2374a974333d1ae645
malaksboui <sboui.95malak@yahoo.com>
2023-03-07 18:10:53 +0100

    done

--- log.js
+++ log.js
@@ -1,8 +1,24 @@
-var login= ["alaa farhati"]
- var signup=function(){
-    for (var i=0;i<login.length;i++){
-        if ($("#log1").val()===login[i]){
-        alert("loged")
-    }
- }
-}
+var username = document.getElementById('username')
+var password = document.getElementById('password')
+var form = document.getElementById('form')
+var errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (event) => {
+  var  messages = []
+  if (username.value === '' || username.value == null) {
+    messages.push('username is required')
+  }
+
+  if ( 8 <= password.value.length <= 15) {
+    messages.push('Password accepted')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be password')
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+  }
+})
